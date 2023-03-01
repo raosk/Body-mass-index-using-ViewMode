bc3fac720ae2aa3b1acec6d0e03fa2182ce2705d
raosk <oskari.rantamaula@gmail.com>
2023-03-01 23:41:35 +0200

    Fixes to UI

--- app/src/main/java/com/example/bmiusingviewmodel/MainActivity.kt
+++ app/src/main/java/com/example/bmiusingviewmodel/MainActivity.kt
@@ -5,16 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.bmiusingviewmodel.ui.theme.BMIUsingViewModelTheme
 import com.example.bmiusingviewmodel.BmiViewModel
@@ -37,20 +43,33 @@ fun Bmi(bmiViewModel: BmiViewModel = viewModel()) {
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         Text(
-            text = stringResource(R.string.title)
+            text = stringResource(R.string.title),
+            color = MaterialTheme.colors.primary,
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 20.dp, bottom = 12.dp),
         )
-        TextField(
+        OutlinedTextField(
             value = bmiViewModel.heightInput, // Display value on textField.
             onValueChange = { bmiViewModel.changeHeightInput(it) }, // Update value if changed.
             label = {Text(text = stringResource(R.string.Height))},
             singleLine = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 6.dp),
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
         )
-        TextField(
+        OutlinedTextField(
             value = bmiViewModel.weightInput, // Display value on textField.
             onValueChange = { bmiViewModel.changeWeightInput(it) }, // Update value if changed.
             label = {Text(text = stringResource(R.string.Weight))},
             singleLine = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 6.dp),
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
         )
         Text(
